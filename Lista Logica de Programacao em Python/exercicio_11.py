2bd002e2b2e462a9b3926b89557c5bedf2f8812d
angelammoliv-maker <angelammoliv@gmail.com>
2026-04-16 11:48:06 -0300

    Add files via upload

---
+++ Lista Logica de Programacao em Python/exercicio_11.py
@@ -0,0 +1,16 @@
+
+prova1 = float(input("informe a nota da primeira prova: "))
+prova2 = float(input("informe a nota da segunda prova: "))
+
+bimestre1 = prova1 + prova2
+
+prova3 = float(input("informe a nota da terceira prova: "))
+prova4 = float(input("informe a nota da quarta prova: "))
+
+bimestre2 = prova3 + prova4
+
+semestre = bimestre1 + bimestre2 /2
+
+print(f"a nota semestral é: {semestre}")
+
+     
